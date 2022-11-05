3c2fb94f39b273f740e1a713917cd857beeead67
LolaBiscay <lbiscay@udesa.edu.ar>
2022-11-05 11:18:11 -0300

    Menu Nagivation

--- src/Navigation/MainNavigation.js
+++ src/Navigation/MainNavigation.js
@@ -3,9 +3,10 @@ import React from 'react'
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import TabNavigation from './TabNavigation';
 import LoginScreen from '../screens/Login/LoginScreen'
 import Register from "../screens/Register/Register";
+import Home from '../screens/Home/Home'
 
 const Stack = createNativeStackNavigator()
 
@@ -28,9 +29,17 @@ function MainNavigation (){
                     headerShown: false 
                 }}
                 />
+                <Stack.Screen
+                name='TabNavigation'
+                component={TabNavigation}
+                // options={{
+                //     headerShown: false
+                // }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     )
 }
 
 export default MainNavigation 
+
